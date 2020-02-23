002705f0005458a2a3c6fc0e242746553eb5a6b9
George Duffett <george.duffett@me.com>
2020-02-23 11:44:15 +0000

    Use np.datetime64

--- Price Observer.py
+++ Price Observer.py
@@ -4,14 +4,16 @@ import numpy as np
 item_numbers = np.loadtxt("Item Numbers.txt", dtype=int)
 year, month, day, hour, minute, second, *prices = np.loadtxt('Price Data.txt', unpack=True)
 
-#here we remake the datetime because I don't know how else to do it
-times = [(str(y)+","+str(m)+","+str(d)+":"+str(h)+":"+str(mi)) for y, m ,d ,h, mi in (zip(year, month, day, hour, minute))]
+# Format dates and times to ISO 8601 
+dates = (f'{y:.0f}-{m:02.0f}-{d:.0f}' for y, m, d in zip(year, month, day))
+times = (f'{h:02.0f}:{m:02.0f}:{s:02.0f}' for h, m, s in zip(hour, minute, second))
+
+# Make use of numpy datetime64 datatype
+datetimes = [np.datetime64(f'{d}T{t}') for d, t in zip(dates, times)]
 
 for item, price in zip(item_numbers, prices):
-    plt.plot(times, price, label=item)
+    plt.plot(datetimes, price, label=item)
 plt.xticks(rotation = 90)
 plt.legend()
 plt.tight_layout()
 plt.show()
-
-
